e27ce371609f86dc799c140856434b67ea60c56b
Tazim381 <jucse28.381@gmail.com>
2023-04-01 01:30:01 +0600

    initiate crud app, connect db

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+//port number and other stuff thakbe
+//crud-db is database name 
+const appConfig = {
+  PORT: 5000,
+  MONGODB_URL: "mongodb://localhost:27017/crud-db",
+};
+
+module.exports = appConfig;
---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const appConfig = require("./config/config.js");
+
+//connect mongodb
+async function connetMongoDB() {
+  try {
+    await mongoose.connect(appConfig.MONGODB_URL);
+    console.log("Database connection is successful!");
+  } catch (error) {
+    console.log("Failed to connet the database");
+  }
+}
+
+function initServer() {
+  const app = express();
+  app.listen(appConfig.PORT);
+  connetMongoDB();
+}
+
+initServer();
