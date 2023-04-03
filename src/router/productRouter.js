00ed32b9996faa0ad26cba6ebd8435b0ee2aa295
Tazim381 <jucse28.381@gmail.com>
2023-04-04 02:23:14 +0600

    crud mongoose added'

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -5,5 +5,7 @@ const productRouter = Router();
 
 productRouter.post("/products", ProductController.createProduct);
 productRouter.get("/products", ProductController.getProductList);
-
+productRouter.get("/products/:id", ProductController.getProductById);
+productRouter.patch('/products/:id', ProductController.updateProduct);
+productRouter.delete('/products/:id', ProductController.deleteProductById);
 module.exports = productRouter;
